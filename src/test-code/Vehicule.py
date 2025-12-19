15c8d57c7f68266eec6c1309acb99d74f25eb0e7
BanisherKnn <kenanyilmaz1304@gmail.com>
2025-12-19 16:45:29 +0100

    Ajout d'un fichier pre release, vehicule list,  divers correctifs

--- src/test-code/Vehicule.py
+++ src/test-code/Vehicule.py
@@ -1,9 +1,18 @@
 class Vehicule:
-    def __init__(self, vehicule_id, marque, model, categorie, portes, etat, tarif):
+    def __init__(self, id_vehicule, marque, model, categorie, portes, etat, tarif):
         self.marque = marque
-        self.vehicule_id = vehicule_id
+        self.vehicule_id = id_vehicule
         self.model = model
         self.categorie = categorie
         self.portes = portes
         self.etat = etat
         self.tarif = tarif
+
+class Car(Vehicule):
+    pass
+
+class Motocycle(Vehicule):
+    pass
+
+class Truck(Vehicule):
+    pass
---
+++ src/test-code/VehiculeList.py
@@ -0,0 +1,2 @@
+v1 = Vehicule(1 ,"BMW", "G82", "Berline", "3 portes", "Comme neuf", "200")
+v2 = Vehicule(2 ,"Mercedes", "C220", "Berline", "5 portes", "Bon état", "150")
